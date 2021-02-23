d45971e0201a2e82c72402e542faad5a0f183b31
Atagan <Laura.ramosmartinez@edu.uah.es>
2021-02-23 20:12:06 +0100

    Suma de vector hecha
    El programa que suma un entero a un vector está hecho, espero que funcione bien xdddd

---
+++ Practica0/SumaMatrices.cu
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <helper_cuda.h>
+#include <devide_launch_parameters.h>
+
+__global__ void incrementar_vector(float *a, float b, int N){ //a es el array, b el numero a incrementarlo, N es el numero de elementos total del array.
+	int idThread = blockId.x * blockDim.x + threadhIdx.x; //comprobamos que no haya ningun 
+
+	if(idx < N){ //Esta comprobacion existe por si acaso, idx nunca deberia superar a N
+		a[idThread] = a[idThread] + b;
+	}
+}
+
+
+void main(){
+	cudaError_t error = cudaSuccess;
+
+	//reservamos memoria en el host para el vector
+	size_t size = 8 * sizeof(float);
+	float* host_vector = (float*) malloc(size);
+
+	for (int i = 0; i < 7; i++){
+		host_vector[i] = i;
+	} 
+
+	//reservamos la memoria para el dispositivo AKA: GPU
+	float* device_vector = nullptr;
+	
+	error = cudaMalloc((void**) &device_vector, size);
+
+	if (error != cudaSuccess) { 
+		fprin_(stderr, "addWithCuda failed!");
+		return 1; 
+	}
+
+	//Copiar memoria de host a device
+	error = cudaMemcpy(device_vector, host_vector, size, cudaMemcpyHostToDevice);
+
+	if (cudaStatus != cudaSuccess) { 
+		fprin_(stderr, "addWithCuda failed!");
+		return 1;
+	}
+
+	//Lanzar el kernel que haga la operacion
+
+	int threadsPerBlock = 256;
+	int blocksPerGrid = (7 + threadsPerBlock - 1)/threadsPerBlock;
+	incrementar_vector <<<blocksPerGrid, threadsPerBlock>>> (device_vector, 10, 8);
+
+
+	error=cudaGetLastError();
+	if (cudaStatus != cudaSuccess) { 
+		fprin_(stderr, "addWithCuda failed!");
+		return 1;
+	}
+
+	//Copiamos la memoria del device al host
+	printf("El vector original: [%d, %d, %d, %d, %d, %d, %d, %d]", host_vector[0], host_vector[1], host_vector[2], host_vector[3], host_vector[4], host_vector[5], host_vector[6], host_vector[7]);
+
+	error = cudaMemcpy(host_vector, device_vector,, size, cudaMemcpyDeviceToHost);
+
+	if (cudaStatus != cudaSuccess) { 
+		fprin_(stderr, "addWithCuda failed!");
+		return 1;
+	}
+
+	printf("El vector tras sumarle 10: [%d, %d, %d, %d, %d, %d, %d, %d]", host_vector[0], host_vector[1], host_vector[2], host_vector[3], host_vector[4], host_vector[5], host_vector[6], host_vector[7]);
+
+	error= cudaFree(device_vector);
+	if (cudaStatus != cudaSuccess) { 
+		fprin_(stderr, "addWithCuda failed!");
+		return 1;
+	}
+	free(host_vector);
+
+	printf("todo ok");
+	return(0);
+}
