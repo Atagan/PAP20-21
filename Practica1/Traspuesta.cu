1eaa59b85cf6ff8b16f8a5e812188bb9c233a811
Atagan <Laura.RamosMartinez@edu.uah.es>
2021-03-04 09:12:13 +0100

    What was, will be

--- Practica1/Traspuesta.cu
+++ Practica1/Traspuesta.cu
@@ -1,63 +1,74 @@
 #include <stdio.h>
-#include <cuda_runtime.h>
+#include <stdlib.h> 
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 
 
-__global__ void transponer(int *in, int *out){
+__global__ void transponer(int *in, int *out) {
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	int y = blockIdx.y * blockDim.x + threadIdx.y;
-	int width = gridDim.x * TILE_DIM;
-	out[x][y]=in[y][x];
+	out[y+x*4] = in[x+y*4];
 }
 
 int main(void) {
 	cudaError_t error = cudaSuccess;
-	int h_matrix[4][4];
+	int h_matrix[4][4] = {{0,0,0,0},
+						  {1,1,1,1},
+						  {2,2,2,2},
+						  {3,3,3,3}
+							};
 
-	for(i=0;i<4;i++){
-		for(j=0;j<4;j++){
-			h_matrix[i][j]=i;
-		}
-	}
-	
-	int* d_matrix = nullptr;
-	
-	error=cudaMalloc(&d_matrix, sizeof(int) * 16);
-	
+
+
+	int* d_matrixIn = nullptr;
+
+	error = cudaMalloc(&d_matrixIn, sizeof(int) * 16);
+
 	if (error != cudaSuccess) {
 		printf("addWithCuda failed!");
 		return 1;
 	}
-	
-	error = cudaMemcpy(d_matrix, h_matrix, sizeof(int)*16, cudaMemcpyHostToDevice);
-	
+
+	int* d_matrixOut = nullptr;
+
+	error = cudaMalloc(&d_matrixOut, sizeof(int) * 16);
+
 	if (error != cudaSuccess) {
 		printf("addWithCuda failed!");
 		return 1;
 	}
-	
+
+	error = cudaMemcpy(d_matrixIn, h_matrix, sizeof(int) * 16, cudaMemcpyHostToDevice);
+
+	if (error != cudaSuccess) {
+		printf("addWithCuda failed!");
+		return 1;
+	}
+
 	printf("Matriz original: \n");
-	for (int i = 0; i < 4; i++){
+	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			printf(" %d", h_matrix[(j + (i - 1) * 4)]);
+			printf(" %d", h_matrix[i][j]);
 		}
 		printf("\n");
 	}
-	
+
 	int threadsPerBlock = 256;
 	int blocksPerGrid = (16 + threadsPerBlock - 1) / threadsPerBlock;
-	transponer<<<blocksPerGrid, threadsPerBlock>>>(d_matrix, d_matrix);
-	
-	error = cudaMemcpy(h_matrix, d_matrix, sizeof(int)*16, cudaMemcpyDeviceToHost);
-	
+	transponer <<<blocksPerGrid, threadsPerBlock >>> (d_matrixIn, d_matrixOut);
+
+	error = cudaMemcpy(h_matrix, d_matrixOut, sizeof(int) * 16, cudaMemcpyDeviceToHost);
+
 	if (error != cudaSuccess) {
 		printf("addWithCuda failed!");
 		return 1;
 	}
-	
+
 	printf("Matriz traspuesta: \n");
-	for (int i = 0; i < 4; i++){
+	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			printf(" %d", h_matrix[(j + (i - 1) * 4)]);
+			printf(" %d", h_matrix[i][j]);
 		}
 		printf("\n");
 	}
